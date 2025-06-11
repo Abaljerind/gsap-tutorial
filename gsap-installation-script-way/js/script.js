da9ed257dd5e7c673f892fee1c286c14852358a7
Abaljerind <abaljerinda@gmail.com>
2025-06-11 13:23:54 +0700

    Add example of GSAP usage

---
+++ gsap-installation-script-way/js/script.js
@@ -0,0 +1,12 @@
+//   gsap.to(".box", {
+//     rotation: 360,
+//     duration: 2,
+//   });
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  // gsap code here!
+  gsap.to(".box", {
+    rotation: 360,
+    duration: 2,
+  });
+});
